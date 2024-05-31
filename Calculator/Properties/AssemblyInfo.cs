55b29ddcc4b9e4ef6ffb52e550869e340bb67d80
solde <solde@DESKTOP-BPBO10D>
2024-05-31 12:30:12 +0300

    Добавьте файлы проекта.

---
+++ Calculator/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("Calculator")]
+[assembly: AssemblyDescription("Calculator by Ruben Roy")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Calculator")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+[assembly: ComVisible(false)]
+
+
+[assembly: Guid("3e27ac15-b733-4bf8-b319-1bea202a962f")]
+
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
